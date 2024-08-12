f482bd8f9996e013c0bc0aad216e09f4a390909f
lorondo <mralexanderlindsay@gmail.com>
2024-08-11 19:51:38 -0700

    corrected array object
    edited the array objects for types for each pokemon, from "let types =" to "types:"

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ let pokemonList = [
     {
         name: 'Bulbasaur', 
         height: 71.12, //in cm
-        let types = ['grass', 'poison']
+        types: ['grass', 'poison']
     },
     {
         name: 'Ivysaur', 
         height: 99.06, //in cm
-        let types = ['grass', 'poison']
+        types: ['grass', 'poison']
     },
     {
         name: 'Venusaur', 
         height: 200.66, //in cm
-        let types = ['grass', 'poison']
-    };
+        types: ['grass', 'poison']
+    }
 ];
