3458eb18bfd67ec0a5714b79a573cd5439f0f51c
lorondo <mralexanderlindsay@gmail.com>
2024-08-11 19:47:08 -0700

    added in notes
    added in notes on height to denote cm

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 let pokemonList = [
     {
         name: 'Bulbasaur', 
-        height: 71.12, 
+        height: 71.12, //in cm
         let types = ['grass', 'poison']
     },
     {
         name: 'Ivysaur', 
-        height: 99.06, 
+        height: 99.06, //in cm
         let types = ['grass', 'poison']
     },
     {
         name: 'Venusaur', 
-        height: 200.66, 
+        height: 200.66, //in cm
         let types = ['grass', 'poison']
     };
 ];
