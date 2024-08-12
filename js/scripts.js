7000c48a4fabe4c3eefba9c0049164a61204065b
lorondo <mralexanderlindsay@gmail.com>
2024-08-11 19:46:26 -0700

    adding array of objects
    deleting old code and adding in three pokemon objects with their names, height in cm and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-let favoriteFood = 'tacos';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur', 
+        height: 71.12, 
+        let types = ['grass', 'poison']
+    },
+    {
+        name: 'Ivysaur', 
+        height: 99.06, 
+        let types = ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur', 
+        height: 200.66, 
+        let types = ['grass', 'poison']
+    };
+];
