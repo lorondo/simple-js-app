dc8a930135ca73cc2a48310e7d9b5d8f2ebc4b2a
lorondo <mralexanderlindsay@gmail.com>
2024-08-14 16:30:17 -0700

    adding line break
    adding <br> to the end of each printed string so they don't write in just one line

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,9 @@ let pokemonList = [
 ];
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height >= 200) { //checks height and adds additional statement if they are tall
-        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + " cm) - Wow, that's big!");
+        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + " cm) - Wow, that's big!<br>");
     } else {
-        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + " cm) ");
+        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + " cm)<br>");
     }       
 }
  
