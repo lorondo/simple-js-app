5dae609bf6200824b0b10afe5e8711a4a9d56257
lorondo <mralexanderlindsay@gmail.com>
2024-08-14 16:12:53 -0700

    added if statement to loop
    added an if statement that checks the pokemon's heights and if its above a certain height, it remarks how tall it is

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
- 
-    let pokemonList = [
-        {
-            name: 'Bulbasaur', 
-            height: 71.12, //in cm
-            types: ['grass', 'poison']
-        },
-        {
-            name: 'Ivysaur', 
-            height: 99.06, //in cm
-            types: ['grass', 'poison']
-        },
-        {
-            name: 'Venusaur', 
-            height: 200.66, //in cm
-            types: ['grass', 'poison']
-        }
-    ];
-    for (let i = 0; i < pokemonList.length; i++) {
+let pokemonList = [
+    {
+        name: 'Bulbasaur', 
+        height: 71.12, //in cm
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Ivysaur', 
+        height: 99.06, //in cm
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur', 
+        height: 200.66, //in cm
+        types: ['grass', 'poison']
+    }
+];
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height => 200) { //checks height and adds additional statement if they are tall
+        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + " cm) - Wow, that's big!")
+    } else {
         document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + " cm) ");
     }
+        
+}
  
