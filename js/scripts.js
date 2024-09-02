33807fcb64b43a215d86fd3ce090aeb389cba5a9
lorondo <mralexanderlindsay@gmail.com>
2024-09-01 19:13:34 -0700

    added modal
    added a modal button thats supposed to show the name and height and image of a pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,57 @@
 let pokemonRepository = (function() {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+    let modalContainer = document.querySelector('#modal-container');
+
+    function showModal(title, text, img) {
+        modalContainer.innerHTML = '';
+
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+
+        let imageElement = document.createElement("img");
+        imageElement.setAttribute("src", img);
+        imageElement.setAttribute("width", "304");
+        imageElement.setAttribute("height", "228");
+        imageElement.setAttribute("alt", "The pokemon image");
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modal.appendChild(imageElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+    }
+
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+    }
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
+
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            hideModal();
+        }
+    });
     
     function add(pokemon){
         pokemonList.push(pokemon);
@@ -16,24 +67,17 @@ let pokemonRepository = (function() {
         let button = document.createElement('button');
         button.innerText = pokemon.name;
         button.classList.add('pokemon-button');
-
-        button.addEventListener('click', function (event) {
-            showDetails(pokemon);
-          });
-
         listItem.appendChild(button);
         newElement.appendChild(listItem);
+
+        button.addEventListener('click', function() {
+            showDetails(pokemon);
+          });
     }
 
     function showDetails(pokemon) {
-        loadDetails(pokemon).then(function() {
-            showModal(
-                pokemon.name,
-                'Height: ' + pokemon.height * 10 + 'cm',
-                pokemon.imageUrl
-            );
-        });
-    }
+        showModal(pokemon.name, 'Height: ' + pokemon.height * 10 + 'cm', pokemon.imgUrl)
+        }
 
     function loadList() {
         return fetch(apiUrl).then(function (response) {
@@ -56,22 +100,13 @@ let pokemonRepository = (function() {
         return fetch(url).then(function (response) {
             return response.json();
         }).then(function (details) {
-            item.imageUrl = details.sprites.front_default;
+            item.imgUrl = details.sprites.front_default;
             item.height = details.height;
             item.types = details.types;
         }).catch(function (e) {
             console.error(e);
         });
-    }
 
-    function showModal(title, text, img) {
-        let modalTitle = document.querySelector('.modal-title');
-        let modalText = document.querySelector('#pokemon-height');
-        let modalImage = document.querySelector('#pokemon-image');
-
-        modalTitle.innerText = title;
-        modalText.innerText = text;
-        modalImage.src = img;
     }
 
     return {
@@ -90,16 +125,3 @@ pokemonRepository.loadList().then(function() {
     });
 });
 
-function divide (dividend, divisor){
-    if (divisor === 0){
-        return "You're trying to divide by zero."
-    } else {
-        let result = dividend / divisor;
-        return result; 
-    }   
-}
-
-console.log(divide(4, 2));
-console.log(divide(7, 0));
-console.log(divide(1, 4));
-console.log(divide(12, -3));
